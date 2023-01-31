6b968574efaf053e8f30044ed3bb828c4f127297
Sachin Beniwal <beniwalsachin81918@gmail.com>
2023-01-31 20:15:16 +0530

    adding mathematical operator file

--- index.js
+++
@@ -1,6 +0,0 @@
-let name1="Masai School";//data is string
-
-let name2="A Transformation in Education";
-
-console.log(name1);
-console.log(name2);
---
+++ problem3.js
@@ -0,0 +1,5 @@
+let a="26";
+let b=26;
+let c=12;
+console.log(a+b+c);
+
